903d2eae40c891c762857c40f5bd316e34e08fbc
Mycaelly Sampaio <47608182+mycaellysampaio@users.noreply.github.com>
2021-07-23 21:41:44 -0300

    Add files via upload

---
+++ server.php
@@ -0,0 +1,18 @@
+<?php
+//FAça um programa que liste o conteudo dos arrays associativos acima. 
+//Use um iterador para imprimir esses arrays associativos.Não me envie código, envie o link do github.
+
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";
+echo $_ENV["USERNAME"]];
+?>
